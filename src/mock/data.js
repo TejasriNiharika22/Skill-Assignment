7185b7bf90382efa0148875a8362e4e3b39ae243
TejasriNiharika22 <84013826+TejasriNiharika22@users.noreply.github.com>
2022-08-22 20:08:02 +0530

    Update data.js

--- src/mock/data.js
+++ src/mock/data.js
@@ -1,44 +1,44 @@
 const mock = {
     textMatter: "Enter the Name of the segment",
     heading: "Skill Assessment",
-    saveSegment:"Save Segment",
+    saveSegment: "Save Segment",
     modalPopup: "Popup",
     placeholder: "Name of the Segment",
     saveButton: "Save the Segment",
     cancelButton: "Cancel",
-    regionDropDown:[
+    listOfOptions: [
         {
-            id:"",
-            name:"Add schema to segment",
-            disabled : true
+            id: "",
+            name: "Add schema to segment",
+            disabled: true
         },
         {
-            id:"fname",
-            name:"First Name"
+            id: "fname",
+            name: "First Name"
         },
         {
-            id:"lname",
-            name:"Last Name"
+            id: "lname",
+            name: "Last Name"
         },
         {
-            id:"gender",
-            name:"Gender"
+            id: "gender",
+            name: "Gender"
         },
         {
-            id:"age",
-            name:"Age"
+            id: "age",
+            name: "Age"
         },
         {
-            id:"accname",
-            name:"Account Name"
+            id: "accname",
+            name: "Account Name"
         },
         {
-            id:"city",
-            name:"City"
+            id: "city",
+            name: "City"
         },
         {
-            id:"state",
-            name:"State"
+            id: "state",
+            name: "State"
         }
     ],
 }

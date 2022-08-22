3e0d78dc593f1287df9e76f5bf4d4585f59d1d39
TejasriNiharika22 <84013826+TejasriNiharika22@users.noreply.github.com>
2022-08-22 16:16:21 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,16 @@ import React, { Component } from 'react';
 import mock from '../mock/data';
 import { Modal } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
-import { Link } from 'react-router-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 class App extends Component {
   constructor(props) {
     super(props)
-    this.myRef = React.createRef();
     this.state = {
       editing: false,
       show: false,
+      schemaList: [],
+      segmentName: "",
+      currentSchemaItem: {}
     }
   }
   handleModal() {
@@ -21,19 +21,79 @@ class App extends Component {
     this.onEditClick();
     this.handleModal();
   }
-  onEditClick() {
-    this.setState({ editing: true });
+  onEditClick = (value) => {
+  const list = this.props.Content.regionDropDown;
+   const item = list.find((val) => {
+      return val.id === value;
+   });
+  console.log(item);
+    this.setState({ currentSchemaItem: item });
   }
+
   
-  handleDropdownChange(e) {
+  handleDropdownChange = (e) => {
+    console.log(e.target.value);
     e.preventDefault();
+    this.onEditClick(e.target.value);
   }
+
+  handleSegmentNameChange = (e) => {
+    console.log(e.target.value);
+    this.setState({
+      segmentName: e.target.value
+    });
+  }
+
+  removeItem = (item) => {
+    // TODO need to write
+  }
+
+  renderSchemaItem = (item, index) => {
+    const list = this.props.Content.regionDropDown;
+
+    return <div key={index}>
+       <select ref={this.myRef} style={{ textAlign: "center", color: "green", width: "300px" }} value={item.id} onChange={(e) => this.handleDropdownChange(e)}>
+            {list.map((listItem, listIndex) => {
+                return <option key={listIndex} value={listItem.id}>{listItem.name}</option>
+            })}
+       </select>
+       <Button onClick={(item) => { this.removeItem(item)}}>-</Button>
+    </div>
+  }
+
+  renderListItems = () => {
+    const {schemaList = []} = this.state;
+    const schemaItems = schemaList.map((item, index) => {
+      console.log(item);
+      return this.renderSchemaItem(item, index)
+    });
+    return schemaItems;
+  }
+
+  addColumnToSchema = () => {
+    const {currentSchemaItem = {}, schemaList = [] } = this.state;
+    console.log(this.state);
+    if(currentSchemaItem){
+      schemaList.push(currentSchemaItem);
+      this.setState({
+        schemaList: schemaList,
+        currentSchemaItem: {}
+      }, () => {
+        console.log(this.state);
+      });
+    }
+  
+  }
+
+
+
  
   render() {
-    let regionDropDown = this.props.Content.regionDropDown
-    let options = regionDropDown.map((data) =>
-      <option>{data.name}</option>)
-    
+    let regionDropDown = this.props.Content.regionDropDown;
+    let { currentSchemaItem} = this.state;
+    let options = regionDropDown.map((data, index) =>
+      <option key={index} value={data.id}>{data.name}</option>)
+  
     return (
       <div>
         <h1 style={{ textAlign: "center" }}>{this.props.Content.heading}</h1>
@@ -45,23 +105,25 @@ class App extends Component {
           <Modal.Body>
             <p>{this.props.Content.textMatter}</p>
             <input
-              handleOnChange={(e) => this.handleOnChange(e)}
+              onBlur={(e) => this.handleSegmentNameChange(e)}
               type="text"
               placeholder={this.props.Content.placeholder}
               required
               value={this.props.value} />
             <br />
+            {this.renderListItems()}
             <br />
-                <select ref={this.myRef} style={{ textAlign: "center", color: "green", width: "300px" }} onChange={this.handleDropdownChange.bind(this)}>
+                <select ref={this.myRef} style={{ textAlign: "center", color: "green", width: "300px" }} value={currentSchemaItem?.id ?? ""} onChange={(e) => this.handleDropdownChange(e)}>
                   {options}
                 </select>
                 <br />
                 <br />
-                <Router>
                 <ul>
-                    <li><Link to="/">+Add new schema</Link></li>
+                  <Button onClick = {() => {this.addColumnToSchema()}}  variant={"link"} >
+                  +Add new schema
+                  </Button>
                 </ul>
-                </Router>
+
             </Modal.Body>
           <Modal.Footer>
             <Button >{this.props.Content.saveButton}</Button>
